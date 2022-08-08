1449405fdc2c9ba13c9f65db578df1d139416495
arwaalbathi <109977321+arwaalbathi@users.noreply.github.com>
2022-08-08 18:23:35 +0300

    cw1day4

---
+++ day 4 cw 1 .playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var name = "Arwa"
+var birthdayYear = 2008
+var age : Int
+var currentYear = 2022
+
+ age = currentYear - birthdayYear
+
+print("Hello my name is \(name) and my age is \(age) ")
+
+if age >= 14 || age <= 18
+{
+    print ("نعم يمكنك المشاركة في الكويت تبرمج ")
+}
+else
+{
+    print ("لايمكنك المشاركة في الكويت تبرمج")
+}
+
+var textName = "8"
+var number = 4
+var doubleNUmb : Double
+
+
+doubleNUmb = (Double(textName) ?? 0) + Double(number)
+
